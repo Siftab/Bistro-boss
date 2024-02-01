51b447c03f1d0cabf4e42bba117a8da31ce49ac8
Siftab <siiffuuu@gmail.com>
2024-02-01 14:18:06 +0600

    implemented navbar

--- src/MainLayOut/MainLayout.jsx
+++ src/MainLayOut/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const MainLayout = () => {
     return (
         <div className='max-w-6xl mx-auto'>
-           
+           <Navbar></Navbar>
 
             <Outlet></Outlet>
             <Footer></Footer>
