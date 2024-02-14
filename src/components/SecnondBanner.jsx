9ec091e644aae27193a9b4105058c18f8dda73bc
Siftab <siiffuuu@gmail.com>
2024-02-14 09:08:46 +0600

    implemneted

--- src/components/SecnondBanner.jsx
+++ src/components/SecnondBanner.jsx
@@ -3,7 +3,7 @@ import imageOne from './../assets/home/chef-service.jpg'
 
 const SecnondBanner = () => {
     return (
-        <div className='relative' >
+        <div className='relative mt-16'  >
            <img src={imageOne} alt="" className='absolute' />
            <div className='bg-white text-center max-w-5xl mx-auto border md:py-24 md:px-40 absolute lg:top-20  lg:left-32  '>
            <h3 className='text-5xl'> Bistro Boss</h3>
