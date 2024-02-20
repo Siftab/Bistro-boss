b80d76f7b5c515cf5de93bee727eb11610c4087e
Siftab <siiffuuu@gmail.com>
2024-02-21 00:00:19 +0600

    price tag added

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const FoodCard = ({item}) => {
-    const {name,recipe,image,_id}=item;
+    const {name,recipe,image,_id,price}=item;
     
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="">
           <img src={image} alt="Shoes" className="rounded-xl" />
         </figure>
+        <p className='bg-black text-white absolute right-0 mt-5 mr-6 px-4 py-2'>${price}</p>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
