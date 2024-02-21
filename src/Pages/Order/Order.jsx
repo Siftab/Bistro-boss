08bc80c960b7c1817690ab377b69b331f63073aa
Siftab <siiffuuu@gmail.com>
2024-02-21 11:36:56 +0600

    bug fixed with tab accessing on route

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import CoverImg from '../../components/CoverImg';
 import orderIMG from '../../assets/shop/banner2.jpg'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import FoodCard from '../../components/FoodCard';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import OrderTab from '../../components/OrderTab';
 
 const Order = () => {
+  const categories= ["salads","pizza","soups","desserts","drink"]
+    const {title} =useParams();
+    const [tabIndex,setTabIndex]=useState(categories.indexOf(title));
+
+    // if(title){
+    //   console.log("find category",categories.indexOf(title))
+    //   setTabIndex("find category",categories.indexOf(title))
+    // }
+
+
     const menu = useLoaderData();
     const desserts=  menu.filter(item => item.category === "dessert")
     const pizza= menu.filter(item => item.category === "pizza")
@@ -15,7 +24,8 @@ const Order = () => {
     const soup= menu.filter(item => item.category === "soup")
     const drinks= menu.filter(item => item.category === "drinks")
     
-    const [tabIndex,setTabIndex]=useState(0);
+    
+    console.log(title)
     return (
         <div>
             <CoverImg img={orderIMG} heading="Shop" subHeading=''></CoverImg>
