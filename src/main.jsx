71131ed9098188ce154e97c64a410986b0b3f0dd
Siftab <siiffuuu@gmail.com>
2024-02-29 00:57:01 +0600

    basic setup tanstack query updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,24 @@ import { RouterProvider } from 'react-router-dom'
 import { myRoutes } from './Routes/Routes.jsx'
 import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './Providers/AuthProvider.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={myRoutes}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <RouterProvider router={myRoutes}></RouterProvider>
 
-      </HelmetProvider>
+        </HelmetProvider>
+      </QueryClientProvider>
+
     </AuthProvider>
 
   </React.StrictMode>,
