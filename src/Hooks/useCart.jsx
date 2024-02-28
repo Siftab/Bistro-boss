8544efa52b98ef871d667d21afcac33dfc2c2e7b
Siftab <siiffuuu@gmail.com>
2024-02-29 02:02:55 +0600

    transtack loading

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,6 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxios from './useAxios';
 
 const useCart = () => {
+    const AxiosSeceure=useAxios();
+    const {data:cart}=useQuery({
+        queryKey:['cart'],
+        queryFn: async ()=>{
+            const res= AxiosSeceure.get('/carts')
+            return res.data;
+
+        }
+    })
+
+    return [cart]
     
 };
 
