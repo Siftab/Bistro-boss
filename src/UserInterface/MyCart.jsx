61f4c261909ddd69a6f404735681c6a51de53fb2
Siftab <siiffuuu@gmail.com>
2024-03-04 10:26:46 +0600

    this is pushed

--- src/UserInterface/MyCart.jsx
+++ src/UserInterface/MyCart.jsx
@@ -10,18 +10,22 @@ const MyCart = () => {
     // const totalPrice = cart.reduce((sum,item)=>{sum+item.price ;
     //                                                         },0)
     cart.map(item=>cartID.push(item.itemId))
+    const totalPrice= cartItemsDetails.reduce((sum,item)=>{return sum+item.price
+    },0)
    useEffect(()=>{
     AxiosSecure.post('/cart/information',cartID)
    .then(res=>{setCartItemDetails(res.data)
                     })
    },[])
    console.log('details image',cartItemsDetails)
+   console.log('total',totalPrice)
+
 
 
     return (
         <div>
             <h1 className='text-4xl'>total Order:{cart.length}</h1>
-            <h1 className='text-4xl'>total price:{}</h1>
+            <h1 className='text-4xl'>total price:{totalPrice}</h1>
             <button className='btn btn-primary'>pay</button>
 
         </div>
